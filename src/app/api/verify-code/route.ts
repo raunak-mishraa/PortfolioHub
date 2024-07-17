7f9aabeb3c69c536af6eb604983ed3743ec606e4
raunak mishra <raunakmishra6300@gmail.com>
2024-07-17 15:29:46 +0530

    add verify-code route

---
+++ src/app/api/verify-code/route.ts
@@ -0,0 +1,48 @@
+import dbConnect from "@/lib/dbConnect";
+import UserModel from "@/models/userModel";
+import { NextRequest, NextResponse } from "next/server";
+
+export async function GET(request: NextRequest) {
+    await dbConnect();
+    try {
+        const { username, code } = await request.json();
+        const user = await UserModel.findOne({ username });
+        if (!user) {
+            return NextResponse.json(
+                { success: false, message: 'User not found' },
+                { status: 404 }
+            );
+        }
+
+        const isCodeValid = user.verifyCode === code;
+        const isCodeNotExpired = new Date(user.verifyCodeExpiry) > new Date();
+
+        if (isCodeValid && isCodeNotExpired) {
+            user.isVerified = true;
+            await user.save();
+      
+            return Response.json(
+              { success: true, message: 'Account verified successfully' },
+              { status: 200 }
+            );
+        }
+        else if(!isCodeNotExpired){
+            return NextResponse.json(
+                { success: false, message: 'Verification code has expired' },
+                { status: 400 }
+            );
+        }
+        else{
+            return NextResponse.json(
+                { success: false, message: 'Invalid verification code' },
+                { status: 400 }
+            );
+        }
+    } catch (error) {
+        console.error('Error verifying user:', error);
+        return NextResponse.json(
+          { success: false, message: 'Error verifying user' },
+          { status: 500 }
+        );
+    }
+}
