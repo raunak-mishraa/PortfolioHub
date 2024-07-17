38187660acec1095c2cb50f610c789010f1154eb
raunak mishra <raunakmishra6300@gmail.com>
2024-07-17 13:14:57 +0530

    implement zod validation & write user_availability route

---
+++ src/app/profile/page.tsx
@@ -0,0 +1,38 @@
+import Image from 'next/image'
+import React from 'react'
+import test from '@/components/test.png' 
+import profie_line from '../../../public/profile_line.svg'
+import { FaLinkedinIn } from "react-icons/fa";
+import { FaXTwitter } from "react-icons/fa6";
+
+function page() {
+  return (
+    <div className='py-32 px-10 container'>
+
+      {/* profile section */}
+      <div className='flex  flex-col items-center justify-center gap-y-5'>
+        <div className='flex flex-col items-center'>
+          <div className='w-28 h-28 border border-n-5 p-3 overflow-hidden rounded-full'>
+            <Image 
+              src={test} 
+              alt='avatar' 
+              className='w-full h-full rounded-full object-cover'/>
+          </div>
+          <div className='-m-0.5'>
+            <Image src={profie_line} alt='vector' className='animate-pulse'/>
+          </div>
+          <div className='flex gap-x-12'>
+            <div className='w-10 h-10 border flex items-center justify-center rounded-full border-n-5'>
+            <FaLinkedinIn className='text-[#757185]'/>
+            </div>
+            <div className='w-10 h-10 border flex items-center justify-center rounded-full border-n-5'>
+            <FaXTwitter className='text-[#757185]'/>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default page
